9343206e5f0872d8674eb5ab626903bcb5fe1381
Juan Quijano <listas@juanquijano.es>
2024-09-10 11:27:44 +0200

    Primer cambio en el código

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,6 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/* See https://aka.ms/new-console-template for more information
+otro comentario*/
 using System.Drawing;
 
-
-Console.BackgroundColor = ConsoleColor.Blue;
-Console.WriteLine("Holaaaa, World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
