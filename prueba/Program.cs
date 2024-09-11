6b606fe313ab5a594188f0b1b74e32aa97dad584
Juan Quijano <listas@juanquijano.es>
2024-09-11 11:39:55 +0200

    Variables

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,7 +1,27 @@
-﻿/* See https://aka.ms/new-console-template for more information
-otro comentario*/
-using System.Drawing;
+﻿/*Console.WriteLine("Hola mundo");
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);*/
 
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
+
+
+using System.Reflection.Metadata;
+
+var firstName ="Blob";
+var messsagesNumber =1;
+var celsius = 27.5;
+
+firstName = "Bob";
+Console.Write("Hello, ");
+Console.Write(firstName);
+Console.Write("!");
+Console.Write("You have ");
+Console.Write(messsagesNumber);
+Console.Write(" messages in your inbox. ");
+Console.Write("The temperature is ");
+Console.Write(celsius);
+Console.WriteLine(" celsius.");
+
+
+Console.Write("Hello, " +  firstName + "! You have " + messsagesNumber + " messages in your inbox. The temperature is " + celsius + " celsius.");
