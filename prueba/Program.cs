1a4cda8ae9e7842294149bca29cc9ed21074078b
Juan Quijano <listas@juanquijano.es>
2024-09-16 12:27:08 +0200

    Declaración de matrices

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,52 +1,12 @@
-﻿Random random = new Random();
+﻿Console.Clear();
 
-var discountPercentage = 10;
-var continua =  true;
+string[] fraudulentOrderIDs = ["A123", "B456", "C789","D453"];
+int[] inventory = { 200, 450, 700, 175, 250 };
 
-while (continua)
+foreach (var item in inventory)
 {
-    var daysUntilExpiration = random.Next(12);
-
-
-    Console.Clear();
-
-    Console.WriteLine("Numero de dias para que expire: " + daysUntilExpiration);
-    if (daysUntilExpiration > 5)
-    {
-        Console.WriteLine("Tu suscripción está a punto de expirar. Renuevela ahora!");
-    }
-    else if (daysUntilExpiration <= 5 && daysUntilExpiration > 1)
-    {
-        Console.WriteLine("Tu suscripción está expira en " + daysUntilExpiration + " dias.");
-    }
-    else if (daysUntilExpiration == 1)
-    {
-        Console.WriteLine("Tu suscripción expira hoy \n Renueva y ahorrate un " + discountPercentage + "%");
-    }
-    else
-    {
-        Console.WriteLine("Tu suscripción ha expirado");
-    }
-
-
-    switch (daysUntilExpiration)
-    {
-        case int n when (n > 5):
-            Console.WriteLine("Tu suscripción está a punto de expirar. Renuevela ahora!");
-            break;
-        case int n when (n <= 5 && n > 3):
-            Console.WriteLine("Tu suscripción está expira en " + daysUntilExpiration + " dias.");
-            break;
-        case int n when (n == 1):
-            Console.WriteLine("Tu suscripción expira hoy \n Renueva y ahorrate un " + discountPercentage + "%");
-            break;
-        default:
-            Console.WriteLine("Tu suscripción ha expirado");
-            break;
-    }
-
-    var tecla = Console.ReadKey();
-    if(tecla.KeyChar == 'x')
-    {continua = false;}
-
+       Console.WriteLine(item); 
 }
+
+var estoEsUnaLista = new List<string>();
+estoEsUnaLista.Add("uno");  
