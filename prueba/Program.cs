de54f1ef6a5449a580136f3588617875152510a7
Juan Quijano <listas@juanquijano.es>
2024-09-18 13:02:15 +0200

    Primera solución

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,9 +1,39 @@
-﻿Console.Clear();
-for (int i = 0; i <= 100; i++)
-{
-var resultado = i + ((i % 3 == 0) && (i % 5 == 0) ? "- FizzBuzz" :
-                (i % 3 == 0)?"- Fizz": 
-                (i % 5 == 0)?"- Buzz":"");
+﻿var vidaDelHeroe = 10;
+var vidaDelMonstruo = 10;
+var dados = new Random();
+var tirada = 0;
+var atacante = true;
 
-Console.WriteLine(i+ resultado);
-}
+Console.Clear();
+
+do
+{
+    Console.WriteLine($"Ataca el {atacante}");
+
+    tirada = dados.Next(1, 11);
+    Console.Write(" -> saca un " + tirada);
+
+    vidaDelMonstruo -= tirada;
+    Console.WriteLine($" -> le quedan {vidaDelMonstruo} puntos de vida al monstruo");
+
+    if (vidaDelMonstruo >= 0)
+    {
+        Console.WriteLine("Ataca el monstruo");
+        tirada = dados.Next(1, 11);
+        Console.Write(" -> saca un " + tirada);
+
+        vidaDelHeroe -= tirada;
+        Console.WriteLine($" -> le quedan {vidaDelHeroe} puntos de vida al heroe");
+        if (vidaDelHeroe <= 0)
+        {
+            Console.WriteLine("Ha muerto el heroe");
+        }
+    }
+    else
+    { Console.WriteLine("El monstruo ha muerto"); }
+
+    Console.ReadKey();
+
+} while (vidaDelHeroe > 0 && vidaDelMonstruo > 0); 
+
+Console.WriteLine("El combate ha finalizado");
