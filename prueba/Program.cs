731226db366c98d48310078b6fe47be0e42a3140
Juan Quijano <listas@juanquijano.es>
2024-09-18 10:43:26 +0200

    Uso de regiones

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,9 +1,8 @@
-﻿string permission = "Pepe";
-int level = 70;
-
-Console.WriteLine(permission.Contains("Admin") 
-    ? level > 55 ? "Welcome, Super Admin": "Welcome, Admin user" 
-    : "No tienes permisos suficientes");
-
-Console.WriteLine((permission.Contains("Manager") & level > 20 )? "Contact an Admin for access.": "No tienes permisos suficientes");
-
+﻿#region Condicional de suma
+if (true)
+{
+    #region Impresion en consola
+    Console.WriteLine("Hola Mundo");
+    #endregion
+}
+#endregion
