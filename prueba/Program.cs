3e92361df39001c2ad63ce4f05c14b177ce0a513
Juan Quijano <listas@juanquijano.es>
2024-09-19 12:50:24 +0200

    Divisiones con conversiones

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,22 +1,18 @@
-﻿// Desafio Realización de un desafío para combinar valores de matriz de cadenas como cadenas y como enteros
+﻿int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-using System.Runtime.InteropServices.Marshalling;
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+var result1  = (decimal)value1 /value2;
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {Math.Round(result1)}");
 
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-var mensaje = "";
-var total = 0.0;
+var result2  = value2/(decimal)value3;
+// Your code here to set result2
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-foreach (var value in values)
-{
-    if (double.TryParse(value, out double resultado))
-    { total += resultado; }
-    else
-    { mensaje += value; }
-}
-
-Console.Clear();
-Console.WriteLine("Mensaje: " + mensaje);
-Console.WriteLine("Total: " + total);
-
+var result3  = value3/value1;
+// Your code here to set result3
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
 
 
