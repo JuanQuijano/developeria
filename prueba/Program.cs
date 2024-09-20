91809b3bce0eed56ccf22551f7499637049581f3
Juan Quijano <listas@juanquijano.es>
2024-09-20 10:32:20 +0200

    Frase en espejo

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,18 +1,16 @@
-﻿int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
-var result1  = (decimal)value1 /value2;
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {Math.Round(result1)}");
-
-var result2  = value2/(decimal)value3;
-// Your code here to set result2
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-var result3  = value3/value1;
-// Your code here to set result3
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string[] resultado = pangram.Split(" ");
+//string[] reverse = new string[resultado.Length];
+string final = string.Empty;
 
 
+for (int i = 0; i < resultado.Length; i++)
+{
+    /*string palabra = resultado[i];
+    string palabraInvertida = string.Join("", palabra.ToCharArray().Reverse());
+    reverse[i] = palabraInvertida;*/
+    final += string.Join("", resultado[i].ToCharArray().Reverse()) + " ";
+
+}
+
+Console.WriteLine(final);
