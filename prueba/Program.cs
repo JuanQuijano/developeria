7ec99f9b514219e08c6e8e884a128b2493b5f3e0
Juan Quijano <jc_quijano@hotmail.com>
2024-09-27 11:21:16 +0200

    metodo sencillo

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,19 +1,75 @@
-﻿Console.Clear();
-int[] numeros = {1,2,3,4,5};
+﻿using System;
+
+int[] times = { 800, 1200, 1600, 2000 };
+int diff = 0;
+int currentGMT = 0;
+int newGMT = 0;
+
+Console.WriteLine("Enter current GMT");
+AdjusTime();
+ currentGMT = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Current Medicine Schedule:");
+DisplayTime();
+
+Console.WriteLine("Enter new GMT");
+ newGMT = Convert.ToInt32(Console.ReadLine());
+AdjusTime();
+DisplayTime();
 
 
 
-void PrintArrayDeNumeros()
+void AdjusTime()
 {
-    foreach (var numero in numeros)
+    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
     {
-        Console.Write($"{numero} ");
+        Console.WriteLine("Invalid GMT");
     }
+    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+    {
+        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+
+        /* Adjust the times by adding the difference, keeping the value within 24 hours */
+        for (int i = 0; i < times.Length; i++)
+        {
+            times[i] = ((times[i] + diff)) % 2400;
+        }
+    }
+    else
+    {
+        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+
+        /* Adjust the times by adding the difference, keeping the value within 24 hours */
+        for (int i = 0; i < times.Length; i++)
+        {
+            times[i] = ((times[i] + diff)) % 2400;
+        }
+    }
+}
+void DisplayTime()
+{
+    Console.WriteLine("New Medicine Schedule:");
+
+    /* Format and display medicine times */
+    foreach (int val in times)
+    {
+        string time = val.ToString();
+        int len = time.Length;
+
+        if (len >= 3)
+        {
+            time = time.Insert(len - 2, ":");
+        }
+        else if (len == 2)
+        {
+            time = time.Insert(0, "0:");
+        }
+        else
+        {
+            time = time.Insert(0, "0:0");
+        }
+
+        Console.Write($"{time} ");
+    }
+
     Console.WriteLine();
 }
-
-
-
-Console.WriteLine("Contents of Array:");
-PrintArrayDeNumeros();
-
