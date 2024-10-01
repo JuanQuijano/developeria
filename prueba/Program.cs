5e40cc85bbe6cd9f3cbb792789eb298b5e992b4a
Juan Quijano <listas@juanquijano.es>
2024-10-01 11:19:05 +0200

    Crear email para usuarios internos y externos

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,5 +1,47 @@
-﻿Console.Clear();
-var fecha = new DateTime(2024,05,31);
+﻿using System.Runtime.InteropServices;
 
-Console.WriteLine (fecha.AddMonths(1).Ticks);
-Thread.Sleep(1000);
+string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    Console.WriteLine(CreateEmail(i, true));
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    Console.WriteLine(CreateEmail(i, false));
+}
+
+string CreateEmail(int indice, bool IsInternal)
+{
+    var internalDomain = "contoso.com";
+    var externalDomain = "hayworth.com";
+    var email = string.Empty;
+
+
+    if (IsInternal)
+    {
+        var nombre = corporate[indice, 0].Substring(0, 2);
+        var apellido = corporate[indice, 1];
+        email = nombre + apellido + "@" + internalDomain;
+    }
+    else
+    {
+        var nombre = external[indice, 0].Substring(0, 2);
+        var apellido = external[indice, 1];
+        email = nombre + apellido + "@" + externalDomain;
+    };
+
+    return email.ToLower();
+}
