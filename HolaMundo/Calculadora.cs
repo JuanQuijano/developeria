f032be9d6590126be65b3c602831d60c1d2f1911
Juan Quijano <listas@juanquijano.es>
2024-10-08 13:47:52 +0200

    Kata BizzFuzz

--- HolaMundo/Calculadora.cs
+++ HolaMundo/Calculadora.cs
@@ -5,37 +5,38 @@ using System.Security.Cryptography;
 namespace HolaMundo;
 public class Calculadora
 {
-    public int Suma(int numero1, int numero2)
-    {
-        return numero1 + numero2;
-    }
+   public bool IsMultiplo3(int numero)
+   {
+      return numero % 3 == 0;
+   }
 
-    public int? Resta(int numero1, int? numero2)
-    {
-        return numero1-numero2;
-    }
+   public bool IsMultiplo5(int numero)
+   {
+      return numero % 5 == 0;
+   }
 
-        public int? Division(int dividendo, int divisor)
-    {
-        int? resultado = null;
-        if (divisor > 0)
-        {resultado = dividendo/divisor;}
+   public bool Is3and5(int numero)
+   {
+      return (IsMultiplo3(numero) && IsMultiplo5(numero));
+   }
 
-        return resultado;
-    }
-
-
-    /* public int Suma(int numero1, int numero2)
-     {
-         return numero1 + numero2;
-     }
-     public int Resta(int numero1, int numero2)
-     {
-         return numero1 - numero2;
-     }
-     public int Multiplica(int numero1, int numero2)
-     {
-         return numero1 * numero2;
-     }*/
+   public string Imprime(int numero)
+   {
+      var cadena = numero.ToString();
+      if (IsMultiplo3(numero))
+      {
+         cadena = "Fizz";
+      }
+      if (IsMultiplo5(numero))
+      {
+         cadena = "Buzz";
+      }
+      if (Is3and5(numero))
+      {
+         cadena = 
+         "FizzBuzz";
+      }
+      return cadena;
+   }
 
 }
--- HolaMundoTest/CalculadoraTest.cs
+++ HolaMundoTest/CalculadoraTest.cs
@@ -6,185 +6,118 @@ namespace HolaMundo;
 public class CalculadoraTest
 {
     [TestMethod]
-    public void Suma_5_3_y_da_8()
+    public void Le_mando_3_y_responde_true()
     {
         Calculadora calculadora = new Calculadora();
-        var numero1 = 5;
-        var numero2 = 3;
-        var resultadoEsperado = 8;
+        var numero = 3;
+        var resultadoEsperado = true;
 
-        var resultado = calculadora.Suma(numero1, numero2);
+        var resultado = calculadora.IsMultiplo3(numero);
 
         Assert.AreEqual(resultadoEsperado, resultado);
     }
     [TestMethod]
-    public void Suma_2_2_y_da_4()
+    public void Le_mando_2_y_responde_false()
     {
         Calculadora calculadora = new Calculadora();
-        var numero1 = 2;
-        var numero2 = 2;
-        var resultadoEsperado = 4;
+        var numero = 2;
+        var resultadoEsperado = false;
 
-        var resultado = calculadora.Suma(numero1, numero2);
+        var resultado = calculadora.IsMultiplo3(numero);
 
         Assert.AreEqual(resultadoEsperado, resultado);
     }
 
     [TestMethod]
-    public void Suma_menos2_menos2_y_da_menos4()
+    public void Le_mando_5_y_responde_true()
     {
         Calculadora calculadora = new Calculadora();
-        var numero1 = 2;
-        var numero2 = 2;
-        var resultadoEsperado = 4;
+        var numero = 5;
+        var resultadoEsperado = true;
 
-        var resultado = calculadora.Suma(numero1, numero2);
+        var resultado = calculadora.IsMultiplo5(numero);
+
+        Assert.AreEqual(resultadoEsperado, resultado);
+    }
+        [TestMethod]
+    public void Le_mando_4_y_responde_false()
+    {
+        Calculadora calculadora = new Calculadora();
+        var numero = 4;
+        var resultadoEsperado = false;
+
+        var resultado = calculadora.IsMultiplo5(numero);
 
         Assert.AreEqual(resultadoEsperado, resultado);
     }
 
-    [TestMethod]
-    public void Resta_3_menos_2_y_da_1()
-    {
-        Calculadora calculadora = new Calculadora();
-        var numero1 = 3;
-        var numero2 = 2;
-        var resultadoEsperado = 1;
-
-        var resultado = calculadora.Resta(numero1, numero2);
-
-        Assert.AreEqual(resultadoEsperado, resultado);
-    }
-
-    [TestMethod]
-    public void Resta_5_menos_2_y_da_3()
-    {
-        Calculadora calculadora = new Calculadora();
-        var numero1 = 5;
-        var numero2 = 2;
-        var resultadoEsperado = 3;
-
-        var resultado = calculadora.Resta(numero1, numero2);
-
-        Assert.AreEqual(resultadoEsperado, resultado);
-    }
-
-    [TestMethod]
-    public void Resta_menos5_menos_mas2_y_da_menos3()
-    {
-        Calculadora calculadora = new Calculadora();
-        var numero1 = -5;
-        var numero2 = 2;
-        var resultadoEsperado = -7;
-
-        var resultado = calculadora.Resta(numero1, numero2);
-
-        Assert.AreEqual(resultadoEsperado, resultado);
-    }
-
-    [TestMethod]
-    public void Divide_4_entre_2_y_da_2()
-    {
-        Calculadora calculadora = new Calculadora();
-        var dividendo = 4;
-        var divisor = 2;
-        var resultadoEsperado = 2;
-
-        var resultado = calculadora.Division(dividendo, divisor);
-
-        Assert.AreEqual(resultadoEsperado, resultado);
-    }
-
-    [TestMethod]
-    public void Divide_4_entre_0_y_da_null()
-    {
-        Calculadora calculadora = new Calculadora();
-        var dividendo = 4;
-        var divisor = 0;
-        int? resultadoEsperado = null;
-
-        var resultado = calculadora.Division(dividendo, divisor);
-
-        Assert.AreEqual(resultadoEsperado, resultado);
-    }
-
-    [TestMethod]
-    public void Suma2_2_menos_division_por_cero_me_da_null()
-    {
-        Calculadora calculadora = new Calculadora();
-        var sumando1 = 2;
-        var sumando2 = 2;
-        var dividendo = 4;
-        var divisor = 0;
-        int? resultadoEsperado = null;
-        
-        var suma = calculadora.Suma(sumando1, sumando2);
-        var division = calculadora.Division(dividendo, divisor);
-        var resultado = calculadora.Resta(suma, division);
-
-        Assert.AreEqual(resultadoEsperado, resultado);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*public void Suma_5_3_y_da_8()
-    {
-        // Arrange
-        Calculadora calculadora = new Calculadora();
-        int numero1 = 5;
-        int numero2 = 3;
-        int resultadoEsperado = 8;
-
-        // Act
-        int resultado = calculadora.Suma(numero1, numero2);
-
-        // Assert
-        Assert.AreEqual(resultadoEsperado, resultado);
-    }*/
-
-    /* [TestMethod]
-     public void Resta_5_1_y_da_4()
-     {
-         // Arrange
-         Calculadora calculadora = new Calculadora();
-         var numero1 = 5;
-         var numero2 = 1;
-         var resultadoEsperado = 4;
-
-         // Act
-         var resultado = calculadora.Resta(numero1, numero2);
-
-         // Assert
-         Assert.AreEqual(resultadoEsperado, resultado);
-     }
      [TestMethod]
-     public void Multiplica_2_2_y_da_4()
-     {
-         // Arrange
-         Calculadora calculadora = new Calculadora();
-         var numero1 = 2;
-         var numero2 = 2;
-         var resultadoEsperado = 4;
+    public void Le_mando_7_y_responde_false()
+    {
+        Calculadora calculadora = new Calculadora();
+        var numero = 7;
+        var resultadoEsperado = false;
 
-         // Act
-         var resultado = calculadora.Multiplica(numero1, numero2);
+        var resultado = calculadora.Is3and5(numero);
 
-         // Assert
-         Assert.AreEqual(resultadoEsperado, resultado);
-     }*/
+        Assert.AreEqual(resultadoEsperado, resultado);
+    }
+    [TestMethod]
+    public void Le_mando_15_y_responde_true()
+    {
+        Calculadora calculadora = new Calculadora();
+        var numero = 15;
+        var resultadoEsperado = true;
+
+        var resultado = calculadora.Is3and5(numero);
+
+        Assert.AreEqual(resultadoEsperado, resultado);
+    }
+
+        [TestMethod]
+    public void Le_mando_3_y_responde_Fizz()
+    {
+        Calculadora calculadora = new Calculadora();
+        var numero = 3;
+        var resultadoEsperado = "Fizz";
+
+        var resultado = calculadora.Imprime(numero);
+
+        Assert.AreEqual(resultadoEsperado, resultado);
+    }
+
+            [TestMethod]
+    public void Le_mando_5_y_responde_Buzz()
+    {
+        Calculadora calculadora = new Calculadora();
+        var numero = 5;
+        var resultadoEsperado = "Buzz";
+
+        var resultado = calculadora.Imprime(numero);
+
+        Assert.AreEqual(resultadoEsperado, resultado);
+    }
+    
+            [TestMethod]
+    public void Le_mando_1_y_responde_1()
+    {
+        Calculadora calculadora = new Calculadora();
+        var numero = 1;
+        var resultadoEsperado = "1";
+
+        var resultado = calculadora.Imprime(numero);
+
+        Assert.AreEqual(resultadoEsperado, resultado);
+    }
+                [TestMethod]
+    public void Le_mando_15_y_responde_FizzBuzz()
+    {
+        Calculadora calculadora = new Calculadora();
+        var numero = 15;
+        var resultadoEsperado = "FizzBuzz";
+
+        var resultado = calculadora.Imprime(numero);
+
+        Assert.AreEqual(resultadoEsperado, resultado);
+    }
 }
